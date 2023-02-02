a13358d16788c20192edc166d4e96178489142e1
root <amzid@hotmail.fr>
2023-02-02 16:57:04 +0000

    TAsk2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Module for variable annotations in Python
+'''
+
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    Function that returns the floor value of a float with annotations
+    '''
+    return math.floor(n)
