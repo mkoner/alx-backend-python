d47c00156708000f4e6f812d70575707448eb849
root <amzid@hotmail.fr>
2023-02-02 16:40:55 +0000

    Task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Module for variable annotations in Python
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Function that concatenates two strings with annotations
+    '''
+    return str1 + str2
