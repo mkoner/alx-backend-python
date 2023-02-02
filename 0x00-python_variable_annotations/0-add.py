916eecdb4e7ce29052167bfe2f5ac6e254c014af
root <amzid@hotmail.fr>
2023-02-02 16:28:21 +0000

    Task0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Module for variable annotations in Python
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Function that adds two floats with annotations
+    '''
+    return a + b
