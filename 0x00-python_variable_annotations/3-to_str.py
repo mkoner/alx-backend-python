4f2289d969604fccc584ec87ee05008440dcbce3
root <amzid@hotmail.fr>
2023-02-02 17:05:19 +0000

    Task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Module for variable annotations in Python
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Function that returns the string representation of a float with annotations
+    '''
+    return str(n)
