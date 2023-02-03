66e8b514732259b49d737b98ada709b8f3bce3aa
root <amzid@hotmail.fr>
2023-02-03 02:03:05 +0000

    Task9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Module for variable annotations in Python
+'''
+
+from typing import Iterable, Sequence, Tuple, List
+
+
+Lst = Iterable[Sequence]
+SeqInt = Tuple[Sequence, int]
+
+def element_length(lst: Lst) -> List[SeqInt]:
+    return [(i, len(i)) for i in lst]
