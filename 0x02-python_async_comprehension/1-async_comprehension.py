969cc90eb8bf186e68e96055b6fc72820d07dfa4
root <amzid@hotmail.fr>
2023-02-07 15:48:03 +0000

    Task1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Module for 0x02. Python - Async Comprehension
+'''
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Async Comprehensions  """
+    a = [i async for i in async_generator()]
+    return a
